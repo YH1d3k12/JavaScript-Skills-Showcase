7fe97f309104e3ebcd33f3ddb06fcb19676a89c9
djalma <djalmahy@hotmail.com>
2023-08-14 18:00:18 -0300

    Feat: application now loads static files and send html as a response

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Senac_Exercises
 This is a repository containing exercises from the 'Jovem Programador' developer course at Senac.
+
+Use the following command to start the application
+
+npx nodemon index.js
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express, { request } from 'express'; // Import the Express library
+import { fileURLToPath } from 'url'; // Import the fileURLToPath function from the 'url' module
+import path from 'path';
 
 const app = express(); // Create an instance of an Express application
-
-// Define routes and middleware here...
-
 const PORT = process.env.PORT || 3000; // Choose a port for your server
 
-// app.get("/", (req, res) => {
-//     res.send(console.log("Hello World!"));
-// });
+// Convert the current module's URL to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// app.get('/', (_, res) => {
-//     res.json({ message: 'Batata!' });
-// });
+//Middleware to load static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'public/views/home.html');
+    res.sendFile(filePath);
+});
 
 app.get('/api/:id', (req, res) => {
     const nome = req.query.nome; //Vai depois doi params
@@ -28,3 +31,11 @@ app.get('/api/:id', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// app.get("/", (req, res) => {
+//     res.send(console.log("Hello World!"));
+// });
+
+// app.get('/', (_, res) => {
+//     res.json({ message: 'Batata!' });
+// });
