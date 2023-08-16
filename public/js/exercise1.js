cb94465cfb48df82e5322ef8dbd45a48f7307430
djalma <djalmahy@hotmail.com>
2023-08-16 18:35:01 -0300

    Feat: created sum two numbers method

---
+++ public/js/exercise1.js
@@ -0,0 +1,18 @@
+function SumTwoNumbers() 
+{
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+
+    // Checks if method is being clicked
+    console.log("Clicou na função SumTwoNumbers");
+
+    //isNan returns true if the value is not a number, so !isNan returns true if the value is a number
+    if (!isNaN(num1) && !isNaN(num2)) 
+    {
+        const result = num1 + num2;
+        document.getElementById('result').textContent = `Resultado: ${result}`;
+    } else 
+    {
+        document.getElementById('result').textContent = 'Insira dois números válidos';
+    }
+}
