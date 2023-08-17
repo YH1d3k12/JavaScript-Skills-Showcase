0e0923272d3929355627bd0f4d573c3078c81163
YH1d3k12 <djalmahy@hotmail.com>
2023-08-16 22:02:22 -0300

    Feat: added two lists

--- public/js/menu_switch.js
+++ public/js/menu_switch.js
@@ -2,38 +2,102 @@ const body = document.getElementById('body');
 const menu = document.getElementById('menu');
 const menu_list_title = document.getElementById('menu_list_title')
 const toggle_list_button = document.getElementById('toggle_list_button');
+
 const exercises_list = document.getElementById('exercises_list');
 const challenges_list = document.getElementById('challenges_list');
+const extra_list = document.getElementById('extra_list');
+const query_params = document.getElementById('qp_list');
+
+let current_list = 2;
 
 // When the button is clicked it will execute the following code:
 toggle_list_button.addEventListener('click', () => {
 
     // this will change the current list display
-    if (exercises_list.style.display === 'none') 
+    if (current_list === 1) 
     {
+        // change background color for body and menu
         body.style.backgroundColor = 'var(--page-exercise-background-color)';
         menu.style.backgroundColor = 'var(--menu-exercise-background-color)';
+        // change menu list color and text
         menu_list_title.style.color = 'var(--menu-exercise-title-color)';
         menu_list_title.textContent = 'Lista de Exercícios';
+        // change toggle button colors and text
         toggle_list_button.style.borderColor = 'var(--button-exercise-border-color';
         toggle_list_button.style.backgroundColor = 'var(--button-exercise-background-color)';
         toggle_list_button.style.color = 'var(--button-exercise-color)';
+        // show exercise list
         exercises_list.style.display = 'block';
+        // hides the remaining lists
         challenges_list.style.display = 'none';
-
+        extra_list.style.display = 'none';
+        query_params.style.display = 'none';
+        // sets the value of current list
+        current_list = 2;
     } 
-    else 
+    else if (current_list === 2)
     {
+        // change background color for body and menu
         body.style.backgroundColor = 'var(--page-challenge-background-color)';
         menu.style.backgroundColor = 'var(--menu-challenge-background-color)';
+        // change menu list color and text
         menu_list_title.style.color = 'var(--menu-challenge-title-color)';
         menu_list_title.textContent = 'Lista de Desafios';
+        // change toggle button colors and text
         toggle_list_button.style.borderColor = 'var(--button-challenge-border-color';
         toggle_list_button.style.backgroundColor = 'var(--button-challenge-background-color)';
         toggle_list_button.style.color = 'var(--button-challenge-color)';
-        exercises_list.style.display = 'none';
+        // show challenge list
         challenges_list.style.display = 'block';
+        // hides the remaining lists
+        exercises_list.style.display = 'none';
+        extra_list.style.display = 'none';
+        query_params.style.display = 'none';
+        // sets the value of current list
+        current_list = 3;
     }
-
+    else if (current_list === 3)
+    {
+        // change background color for body and menu
+        body.style.backgroundColor = 'var(--page-extra-background-color)';
+        menu.style.backgroundColor = 'var(--menu-extra-background-color)';
+        // change menu list color and text
+        menu_list_title.style.color = 'var(--menu-extra-title-color)';
+        menu_list_title.textContent = 'Lista Extra';
+        // change toggle button colors and text
+        toggle_list_button.style.borderColor = 'var(--button-extra-border-color';
+        toggle_list_button.style.backgroundColor = 'var(--button-extra-background-color)';
+        toggle_list_button.style.color = 'var(--button-extra-color)';
+        // show challenge list
+        extra_list.style.display = 'block';
+        // hides the remaining lists
+        exercises_list.style.display = 'none';
+        challenges_list.style.display = 'none';
+        query_params.style.display = 'none';
+        // sets the value of current list
+        current_list = 4;
+    }
+    else 
+    {
+        // change background color for body and menu
+        body.style.backgroundColor = 'var(--page-qp-background-color)';
+        menu.style.backgroundColor = 'var(--menu-qp-background-color)';
+        // change menu list color and text
+        menu_list_title.style.color = 'var(--menu-qp-title-color)';
+        menu_list_title.textContent = 'Query Params';
+        // change toggle button colors and text
+        toggle_list_button.style.borderColor = 'var(--button-qp-border-color';
+        toggle_list_button.style.backgroundColor = 'var(--button-qp-background-color)';
+        toggle_list_button.style.color = 'var(--button-qp-color)';
+        // show challenge list
+        query_params.style.display = 'block';
+        // hides the remaining lists
+        exercises_list.style.display = 'none';
+        challenges_list.style.display = 'none';
+        extra_list.style.display = 'none';
+        // sets the value of current list
+        current_list = 1;
+    }
+    // testing if toggle button is working when clicked
     console.log("FUNFOU");
 });
