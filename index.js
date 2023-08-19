ae8e35fd34a1954d412dd44b5a83076d60dc041e
YH1d3k12 <djalmahy@hotmail.com>
2023-08-18 22:07:37 -0300

    Feat: created routes

--- index.js
+++ index.js
@@ -1,69 +1,17 @@
 import express, { request } from 'express'; // Import the Express library
-import { fileURLToPath } from 'url'; // Import the fileURLToPath function from the 'url' module
-import path from 'path';
-import { SumTwoNumbers } from './public/js/query_params_script/query1.js';
-
+import query1_routes from './routes/query1.js';
+import views_routes from './routes/views_router.js';
 
 const app = express(); // Create an instance of an Express application
 const PORT = process.env.PORT || 3000; // Choose a port for your server
-
-// Convert the current module's URL to a file path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 //Middleware to load static files
 app.use(express.static('public'));
 //Middleware which allows the request of json files
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    const filePath = path.join(__dirname, 'public/views/home.html');
-    res.sendFile(filePath);
-});
-
-app.get('/api/:id', (req, res) => {
-    const nome = req.query.nome; //params Goes afters params
-    const id = req.params.id; //goes before query
-
-    res.json
-    ({ 
-        message: `Você passou o queryParam ${nome} `+
-        `Você passou o id:${id}` 
-    });
-});
-
-// app.get('/views/query1', (req, res) => {
-//     let num1 = parseFloat(req.query.num1);
-//     let num2 = parseFloat(req.query.num2);
-
-//     if (isNaN(num1) || isNaN(num2))
-//     {
-//         res.status(501).json({
-//             message: `Ensira dois números`
-//         });
-//         return;
-//     }
-//     else
-//     {
-//         res.json({
-//             message: `A partir do GET Resultado: ${num1 + num2}`
-//         });
-//     }
-// });
-
-app.post('/views/query1', (req, res) => {
-    const result = SumTwoNumbers
-    (
-        parseFloat(req.body.num1), 
-        parseFloat(req.body.num2)
-    )
-    
-    res.status(200).json
-    ({
-        message: `A partir do Post Resultado: ${result}`
-    });
-
-});
+app.use('/query1', query1_routes);
+app.use('/views', views_routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ routes/views_router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const views_router = express.Router();
+
+// Convert the current module's URL to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+views_router.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'public/views/home.html');
+    res.sendFile(filePath);
+});
+
+export default views_router;
