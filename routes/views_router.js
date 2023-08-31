ec6e02690dce95141c6cfd7e099551222ed302c2
djalma <djalmahy@hotmail.com>
2023-08-31 10:38:55 -0300

    Chore: change home.html to index.html for github pages

--- routes/views_router.js
+++ routes/views_router.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 views_router.get('/', (req, res) => {
-    const filePath = path.join(__dirname, '../public/views/home.html');
+    const filePath = path.join(__dirname, '../index.html');
     res.sendFile(filePath);
 });
 
