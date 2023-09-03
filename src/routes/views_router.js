809624813f705a64dc42fa341143b52ba87beca9
djalma <djalmahy@hotmail.com>
2023-09-03 14:33:15 -0300

    Refactor: folder structure

--- src/routes/views_router.js
+++ src/routes/views_router.js
@@ -10,7 +10,7 @@ const views_router = express.Router();
 // const __dirname = path.dirname(__filename);
 
 views_router.get('/', (req, res) => {
-    const filePath = path.join(__dirname, '../public/views/home.html');
+    const filePath = path.join(__dirname, '../../public/views/home.html');
     res.sendFile(filePath);
 });
 
